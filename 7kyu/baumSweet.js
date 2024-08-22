057c95c582a7db2c3fbe761117b7b1396659f64c
kubaVencel <105190657+KubaVencel@users.noreply.github.com>
2024-08-22 17:56:29 +0200

    added solution for The Baum-Sweet sequence codewars kata

---
+++ 7kyu/baumSweet.js
@@ -0,0 +1,29 @@
+/*
+
+The Baum-Sweet sequence
+
+Description:
+
+Wikipedia: The Baum–Sweet sequence is an infinite automatic sequence of 0s and 1s defined by the rule:
+
+bn = 1 if the binary representation of n contains no block of consecutive 0s of odd length;
+bn = 0 otherwise;
+
+for n ≥ 0.
+
+Define a generator function baumSweet that sequentially generates the values of this sequence.
+
+It will be tested for up to 1 000 000 values.
+
+Note that the binary representation of 0 used here is not 0 but the empty string ( which does not contain any blocks of consecutive 0s ).
+
+*/
+
+function* baumSweet() {
+  yield 1;
+  let i = 0
+  while(true){
+    ++i
+    yield i.toString(2).split(/1/g).some($ => $.length % 2) ? 0 : 1
+  }
+}
