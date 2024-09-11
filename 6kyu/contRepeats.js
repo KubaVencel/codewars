3b1d8a533b4feb3f4c6339b9199147d4fde405da
Jakub Vencel <j.vencel27@proton.me>
2024-09-11 16:21:49 +0200

    added solution fot Count Repeats codewars kata

---
+++ 6kyu/contRepeats.js
@@ -0,0 +1,24 @@
+/*
+
+Count Repeats
+
+Description:
+
+Write a function that returns the count of characters that have to be removed in order to get a string with no consecutive repeats.
+
+Note: This includes any characters
+Examples
+
+'abbbbc'  => 'abc'    #  answer: 3
+'abbcca'  => 'abca'   #  answer: 2
+'ab cca'  => 'ab ca'  #  answer: 1
+
+*/
+
+function countRepeats(str) {
+  let arr = str.split('');
+  
+  arr = arr.filter((char, idx) => char !== arr[idx + 1]);
+  
+  return str.length - arr.length;
+}
