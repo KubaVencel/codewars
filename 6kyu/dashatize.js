2f8ae9742c04fea10927211ebd093c1b938b197e
Jakub Vencel <j.vencel27@proton.me>
2024-10-18 21:00:01 +0200

    addded solution for Dashatize it codewars kata

---
+++ 6kyu/dashatize.js
@@ -0,0 +1,21 @@
+/*
+
+Dashatize it
+
+Description:
+
+Given an integer, return a string with dash '-' marks before and after each odd digit, but do not begin or end the string with a dash mark.
+
+Ex:
+
+274 -> '2-7-4'
+6815 -> '68-1-5'
+
+*/
+
+function dashatize(num) {
+  return String(num)
+    .replace(/([13579])/g, "-$1-")
+    .replace(/--+/g, "-")
+    .replace(/(^-|-$)/g, "")
+}
