5467e11915715076635190d4edf5b3d082842529
kubaVencel <105190657+KubaVencel@users.noreply.github.com>
2025-03-27 17:42:48 +0100

    added solution for Smart Traffic Lights codewars kata

---
+++ 6kyu/smartTrafficLight.js
@@ -0,0 +1,46 @@
+/*
+
+Smart Traffic Lights
+
+Description:
+Story
+
+Your friend Bob is working as a taxi driver. After working for a month he is frustrated in the city's traffic lights. He asks you to write a program for a new type of traffic light. It is made so it turns green for the road with the most congestion.
+Task
+
+Given 2 pairs of values each representing a road (the number of cars on it and its name), construct an object whose turngreen method returns the name of the road with the most traffic at the moment of the method call, and clears that road from cars.
+
+After both roads are clear of traffic, or if the number of cars on both roads is the same from the beginning, return an empty value instead.
+Example
+
+t = SmartTrafficLight([42, "27th ave"], [72, "3rd st"])
+t.turngreen()  ==  "3rd st"
+t.turngreen()  ==  "27th ave"
+t.turngreen()  ==  null
+
+t = SmartTrafficLight([10, "27th ave"], [10, "3rd st"])
+t.turngreen()  ==  null
+
+*/
+
+class SmartTrafficLight {
+  constructor(st1, st2) {
+    this.st1 = st1
+    this.st2 = st2
+    this.i = 0
+  }
+  
+  turngreen() {
+    let [a, b] = this.st1
+    let [c, d] = this.st2   
+    this.i++
+    switch(this.i){
+      case 1:
+        return c > a ? d: c == a ? null : b
+      case 2:
+        return c > a ? b : c == a ? null : d
+      default:
+        return null
+    }    
+  }
+}
