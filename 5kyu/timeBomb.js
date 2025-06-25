29354431ab187ffb8bcfeeafb2859333787b88cf
kubaVencel <105190657+KubaVencel@users.noreply.github.com>
2025-06-25 19:26:16 +0200

    added solution for Simple Time Bomb codewars kata

---
+++ 5kyu/timeBomb.js
@@ -0,0 +1,14 @@
+/*
+
+Simple Time Bomb
+
+Description:
+
+A bomb has been set to go off! You have to find the wire and cut it in order to stop the timer. There is a global property that holds the key to which wire to cut. Find that and then you can CutTheWire(wireKey);
+
+*/
+
+console.log(CutTheWire.toString());
+
+const wire = theWire; // Find the wire.
+CutTheWire(this[wire]);
