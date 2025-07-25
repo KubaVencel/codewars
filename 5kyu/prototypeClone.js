66bfa22cd352ea6d7eb50318ac928049dfed9939
kubaVencel <105190657+KubaVencel@users.noreply.github.com>
2025-07-25 23:32:25 +0200

    added solution for Function.prototype.clone codewars kata

---
+++ 5kyu/prototypeClone.js
@@ -0,0 +1,20 @@
+/*
+
+Function.prototype.clone
+
+Without using eval, write a clone method to clone a function's arguments and body into a new function. You don't have to worry about the name of the function; it's impossible to override that property of a function.
+
+var original = function original_name(a, b) { return a + b; };
+
+var cloned = o.clone();
+// cloned == function(a, b) { return a + b; }
+
+So the new cloned function is a completely new object, with its own scope, but effectively behaves the exact same way as the original function.
+
+	NOTE: This kata focuses on ES5 only (function fn() {}), but you get bonus points for making your solution ES6 compatible (fn = (a, b) => a + b)!
+
+*/
+
+Function.prototype.clone = function() {
+	return new Function('return ' + this.toString())();
+};
