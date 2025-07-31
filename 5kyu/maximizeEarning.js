9af88300a6354aaf5227b29ee8249af25ea28815
kubaVencel <105190657+KubaVencel@users.noreply.github.com>
2025-07-31 19:09:44 +0200

    added solution for Maximize your earnings with mandatory breaks codewars kata

---
+++ 5kyu/maximizeEarning.js
@@ -0,0 +1,36 @@
+/*
+
+Maximize your earnings with mandatory breaks
+
+Task
+
+You have a list of daily earnings from a freelance job. You can choose to work or skip those days. You can work for up to k consecutive days, but after that, you must take at least one break day where you earn nothing.
+
+Your task is to determine the maximum total earnings you can achieve while following this rule.
+Inputs
+
+	earnings: an array of non-negative integers ( 0 <= length earnings < 100 ).
+	k: an integer representing the maximum number of consecutive working days ( 1 <= k < 100 ).
+
+Output
+
+	an integer representing the maximum earnings you can achieve while respecting the mandatory break rule.
+
+Example
+
+For earnings = [60, 70, 80, 40, 80, 90, 100, 20], k = 3, return 480. Forgo earning 40 and 20.
+
+For earnings = [45, 12, 78, 34, 56, 89, 23, 67, 91], k = 4, return 460. Forgo earning 12 and 23.
+Credit
+
+Original idea by @daryjoe.
+
+*/
+
+function maximizeEarnings(earnings, k) {
+	let best = Array(k + 1).fill(0);
+	for (let e of earnings)
+		for (let i = 0; i < k; i++)
+			best.push(Math.max(best[best.length - 1], best.shift() + e));
+	return best.pop();
+}
