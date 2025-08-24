975aa76fc18edd15bc69b2e4e0182e27a36e54c8
kubaVencel <105190657+KubaVencel@users.noreply.github.com>
2025-08-24 14:25:20 +0200

    added solution for Hello W... wait what? codewars kata

---
+++ 5kyu/helloWorld.js
@@ -0,0 +1,37 @@
+/*
+
+Hello W... wait what?
+
+In order to stop too much communication from happening, your overlords declare that you are no longer allowed to use certain functionality in your code!
+
+Disallowed functionality:
+
+	Strings
+	Numbers
+	Regular Expressions
+	Functions named "Hello", "World", "HelloWorld" or anything similar.
+	Object keys named "Hello", "World", "HelloWorld" or anything similar.
+
+Without using the above, output the string "Hello World!" to prove that there is always a way.
+
+*/
+
+var helloWorld = function() {
+	var one = +true
+	var two = one + one
+	var four = two + two
+	var eight = four + four
+	var sixteen = eight + eight
+	var thirtytwo = sixteen + sixteen
+	var sixtyfour = thirtytwo + thirtytwo
+	var sp = thirtytwo
+	var excl = thirtytwo + one
+	var H = sixtyfour + eight
+	var e = sixtyfour + thirtytwo + four + one
+	var l = sixtyfour + thirtytwo + eight + four
+	var o = sixtyfour + thirtytwo + sixteen - one
+	var W = sixtyfour + sixteen + eight - one
+	var r = sixtyfour + thirtytwo + sixteen + two
+	var d = sixtyfour + thirtytwo + four
+	return [H, e, l, l, o, sp, W, o, r, l, d, excl].map(function(c) { return String.fromCharCode(c) }).join([])
+}
